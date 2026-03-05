8d87a7eb3930b5572653a8594d167c039e92b5f6
Alexander Lang <Alexander.Lang@it-lang.de>
2026-03-05 14:25:07 +0100

    Add project files.

---
+++ WebRadio/Program.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using NAudio;
+using NAudio.Wave;
+using System.Text.Json;
+
+
+namespace WebRadio
+{    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client1 = new RestClient("https://de1.api.radio-browser.info/");
+            var request = new RestRequest("json/stations/", Method.Get);
+            var response = client1.Post(request);
+            Console.WriteLine(response.StatusCode);
+            Console.WriteLine(response.Content);
+
+
+
+            //var client = new RestClient("http://streamer.psyradio.org:8010/;listen.mp3");
+            //var request = new RestRequest("");
+            //var response = client.Execute(request);
+            //if (response.IsSuccessful)
+            //{
+            //    // Assuming the response contains a stream of audio data
+            //    using (var audioStream = new MemoryStream(response.RawBytes))
+            //    {
+            //        // Use NAudio to play the audio stream
+            //        using (var waveOut = new WaveOutEvent())
+            //        using (var waveStream = new WaveFileReader(audioStream))
+            //        {
+            //            waveOut.Init(waveStream);
+            //            waveOut.Play();
+            //            // Keep the application running while the audio is playing
+            //            while (waveOut.PlaybackState == PlaybackState.Playing)
+            //            {
+            //                Thread.Sleep(100);
+            //            }
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Failed to connect to the radio stream.");
+            //}
+        }
+}
+}
